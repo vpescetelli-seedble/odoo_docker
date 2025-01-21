e1ba3f4d35477eb61138a8c7e074d70a35c4ffb4
Valerio Pescetelli <valeriopescetellinl@gmail.com>
2025-01-21 09:55:49 +0100

    Added right ssl configure

---
+++ srcs/staging/nginx/init-letsencrypt.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Ottieni il certificato
+certbot --nginx \
+  --non-interactive \
+  --agree-tos \
+  --email valerio.pescetelli@seedble.com \
+  --domains samir-staging.seedble.com \
+  --redirect
+
+# Configura il rinnovo automatico in background
+certbot renew --deploy-hook "nginx -s reload" &
