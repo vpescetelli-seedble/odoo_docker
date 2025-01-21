089f8689b24c20416893dcd705cea5ef50950fb4
Valerio Pescetelli <valeriopescetellinl@gmail.com>
2025-01-21 11:19:38 +0100

    Other fix

---
+++ srcs/prod/nginx/init-letsencrypt.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Avvia nginx con i certificati esistenti
+nginx
+
+# Attendi che nginx sia avviato
+sleep 5
+
+# Prova a ottenere il certificato Let's Encrypt
+certbot --nginx \
+    --non-interactive \
+    --agree-tos \
+    --email tuo@email.com \
+    --domains samir.seedble.com \
+    --keep-until-expiring \
+    --staging || true  # Il || true permette di continuare anche se certbot fallisce
+
+# Riavvia nginx in foreground
+exec nginx -g 'daemon off;'
