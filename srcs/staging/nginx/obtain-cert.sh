bfe5710ea65c5536f645af29555ffc4ef9408533
Valerio Pescetelli <valeriopescetellinl@gmail.com>
2025-01-21 11:09:58 +0100

    Try to fix certificate

---
+++ srcs/staging/nginx/obtain-cert.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Avvia nginx in background con certificato temporaneo
+nginx
+
+# Attendi un po' per assicurarsi che nginx sia avviato
+sleep 10
+
+# Ottieni certificato Let's Encrypt
+certbot --nginx \
+    --non-interactive \
+    --agree-tos \
+    --email valerio.pescetelli@seedble.com \
+    --domains samir-staging.seedble.com \
+    --staging  # Rimuovi questa riga quando sei pronto per il certificato reale
+
+# Ferma nginx in background
+nginx -s stop
+
+# Riavvia nginx in foreground
+exec nginx -g 'daemon off;'
